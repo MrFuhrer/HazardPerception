3e40591f5daf8d7b1712bac336b14ba26c4d0885
Giorgi Tabatadze <android.giorgi@gmail.com>
2016-12-12 00:04:55 +0400

    Added score calculation

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,46 @@
 var options = {
-    hazards: 2,
     times: [
-        1,1.5
+        5, 10
     ],
-    step: 1,
-    video: 'M7lc1UVf-VE'
+    stepSize: 1,
+    steps: 5,
+    video: 'GXVZZXL5gts',
+    clicks: 3,
+    score: 5
 };
+
 var player;
 (function(){
     "use strict";
 
     initScripts();
+
+    function testCheckHazards(times) {
+        var score = 0;
+
+        var correctTimes = options.times;
+
+        for(var i = 0; i < times.length; i++) {
+            score+=getScore(times[i]);
+        }
+
+        function getScore(time) {
+            for(var i = 0; i < correctTimes.length; i++) {
+                var start = correctTimes[i],
+                    end = start+(options.stepSize * options.steps);
+
+                if(time >= start && time <= end) {
+                    correctTimes.splice(i,1);
+                    return options.steps - parseInt( (time - start) / options.stepSize);
+                }
+            }
+            return 0;
+        }
+
+        alert("Your score is: "+score);
+
+    }
+
 
     function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
@@ -27,15 +57,36 @@ var player;
                 showinfo: false
             },
             events: {
-                'onReady': onPlayerReady
+                'onReady': onReady
             }
         });
     }
 
-    function onPlayerReady(event) {
+
+    function getHazardClickCount() {
+        return options.clicks;
+    }
+
+    function onReady(event) {
         $(event.target.a).removeAttr("allowfullscreen");
 
+        var hazardTimes = [];
+
+        player.addEventListener('onStateChange', function(event) {
+            if(event.data == YT.PlayerState.ENDED) {
+                testCheckHazards(hazardTimes);
+            }
+        });
+
+        var availableClicks = getHazardClickCount();
+
+        function isAbleToClick() {
+            return availableClicks-->0;
+        }
+
+
         $(".hazardButton").click(function() {
+
             var duration = player.getDuration();
             var time = player.getCurrentTime();
 
@@ -46,11 +97,18 @@ var player;
             };
             hazardExpected(hazardData);
         });
+
+        function hazardExpected(data) {
+            if(isAbleToClick()) {
+                $(".hazardPoints").append($("<div class='hazardPoint' style='left: " + (data.share * 100) + "%'>"));
+                hazardTimes.push(data.time);
+            } else {
+                alert("You have no more tries, please wait while video ends for result");
+            }
+        }
+
     }
 
-    function hazardExpected(data) {
-        console.log(data);
-    }
 
     function initScripts() {
         var tag = document.createElement('script');

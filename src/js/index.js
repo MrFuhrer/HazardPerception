c7f141964b5a5b728a85a6527d01e44c2fd3b53f
Giorgi Tabatadze <android.giorgi@gmail.com>
2016-12-11 22:10:35 +0400

    Initial, simple design and time detection when hazard clicked

---
+++ src/js/index.js
@@ -0,0 +1,67 @@
+var options = {
+    hazards: 2,
+    times: [
+        1,1.5
+    ],
+    step: 1,
+    video: 'M7lc1UVf-VE'
+};
+var player;
+(function(){
+    "use strict";
+
+    initScripts();
+
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            height: '390',
+            width: '640',
+            videoId: options.video,
+            allowfullscreen: 0,
+            playerVars: {
+                controls: 0,
+                origin: "localhost:63343",
+                disablekb: true,
+                fs: false,
+                rel: 0,
+                showinfo: false
+            },
+            events: {
+                'onReady': onPlayerReady
+            }
+        });
+    }
+
+    function onPlayerReady(event) {
+        $(event.target.a).removeAttr("allowfullscreen");
+
+        $(".hazardButton").click(function() {
+            var duration = player.getDuration();
+            var time = player.getCurrentTime();
+
+            var hazardData = {
+                duration: duration,
+                time: time,
+                share: parseInt(duration/time*100)/100
+            };
+            hazardExpected(hazardData);
+        });
+    }
+
+    function hazardExpected(data) {
+        console.log(data);
+    }
+
+    function initScripts() {
+        var tag = document.createElement('script');
+
+        tag.src = "https://www.youtube.com/iframe_api";
+
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }
+
+    window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+
+})();
